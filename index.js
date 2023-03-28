de52f145a1d59d3d75740da118d5f716f0a440ed
John <johnsief12@gmail.com>
2023-03-28 19:15:40 -0400

    added scoreboard

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let player = document.getElementById('player')
-let computer = document.getElementById('computer')
-let rock = document.getElementById('rock').addEventListener('click', ()=>{playRound("rock", getComputerChoice())});
-let paper = document.getElementById('paper').addEventListener('click', ()=>{playRound("paper",getComputerChoice())});
-let scissors = document.getElementById('scissors').addEventListener('click', ()=>{playRound("scissors",getComputerChoice())});
+const player = document.getElementById('player')
+const computer = document.getElementById('computer')
+const rock = document.getElementById('rock').addEventListener('click', ()=>{playRound("rock", getComputerChoice())});
+const paper = document.getElementById('paper').addEventListener('click', ()=>{playRound("paper",getComputerChoice())});
+const scissors = document.getElementById('scissors').addEventListener('click', ()=>{playRound("scissors",getComputerChoice())});
+const result = document.querySelector('.result');
+const content = document.createElement('div');
+const score = document.querySelector('.scoreboard');
+let playerScore = 0;
+let computerScore = 0;
 
   function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
@@ -18,31 +23,65 @@ let scissors = document.getElementById('scissors').addEventListener('click', ()=
   };
 
 
+
   function playRound (playerSelection, computerChoice)  {
     if(playerSelection === computerChoice){
- return console.log( "You tied!")
-  }
+    content.textContent = "You tied!";
+    result.appendChild(content);
+    }
   if(playerSelection === "rock"){
     if(computerChoice === "paper"){
-    return console.log("Computer Wins!");
+        computerScore++
+        updateScore(playerScore, computerScore)
+        player.textContent = `Computer Score:  ${computerScore++} vs  Player Score: ${playerScore}`
+        score.appendChild(player)
     }else{
-    return console.log( "You won!")
+        player.textContent = playerScore++
+        updateScore(playerScore, computerScore)
+        player.textContent = `Player Score: ${playerScore++} vs  Computer Score: ${computerScore}`
+        score.appendChild(player)
     }
   }
     if(playerSelection ==="paper"){
       if(computerChoice === "scissors"){
-      return console.log("You won!")
+        player.textContent = playerScore++
+        updateScore(playerScore, computerScore)
+        player.textContent = `Player Score ${playerScore++} vs  Computer Score: ${computerScore}`
+       score.appendChild(player)
       }else{
-       return console.log( "Computer Won!")
+        computerScore++
+        updateScore(playerScore, computerScore)
+        player.textContent = `Computer Score:  ${computerScore++} vs  Player Score: ${playerScore}`
+        score.appendChild(player)
       }
     }
     if(playerSelection === "scissors"){
       if(computerChoice === "rock"){
-       return console.log("Computer Won!")
+computerScore++
+updateScore(playerScore, computerScore)
+player.textContent = `Computer Score:  ${computerScore++} vs  Player Score: ${playerScore}`
+score.appendChild(player)
       }else {
-         return console.log("You Won!")
+        player.textContent = playerScore++
+        updateScore(playerScore, computerScore)
+        player.textContent = `Player Score: ${playerScore++} v  Computer Score: ${computerScore}`
+        score.appendChild(player)
       }
     }
+
   };
 
+  function updateScore(playerScore, computerScore){
+    if(playerScore === 5){
+        content.textContent = "Player Defeated You!!!!!"
+        player.textContent = `Player Wins!!
+        Player Score:${playerScore++} vs  Computer Score: ${computerScore}`
+        score.appendChild(player)
+
+    }else if(computerScore === 5) {
+        content.textContent = "Computer Defeated You!!!!!"
+         result.appendChild(content);
+         score.appendChild(computer)
+    }
+}
 
